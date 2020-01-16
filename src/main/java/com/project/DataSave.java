04c1209ef5ed408004bfb7beddedf483ff8e024f
Mehdi <mf1378mf@yahoo.com>
2020-01-16 17:47:12 +0330

    sadf

--- src/main/java/com/project/DataSave.java
+++ src/main/java/com/project/DataSave.java
@@ -252,8 +252,9 @@ public class DataSave {
     public Person findPersonIndataBase2(String user, String pass) {
         for(Person i : persons){
             if(i.getUser().equals(user) && i.getEmailAddress().equals(pass)){
-
+                    return i;
             }
         }
+        return null;
     }
 }
